1af0ac4f6378b9be084822cfe4e46c2635a4ee8d
TruongDang23 <quantruong050123@gmail.com>
2023-09-17 16:37:33 +0700

    add image to assets

--- ProjectTourism/Form1.cs
+++ ProjectTourism/Form1.cs
@@ -16,5 +16,11 @@ namespace ProjectTourism
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.pictureBox1.ImageLocation = "../../../Details/HaLong001/AnhChiTiet2.jpg";
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
     }
 }
