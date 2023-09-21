41c0e2484454b9021dc3cc8c3d7017c43d3fdfe9
QuocNguyen332003 <21110741@student.hcmute.edu.vn>
2023-09-21 23:24:48 +0700

    FormTrangChu(Chưa tìm được ảnh backgroud), FormDanhGia, FormQuanLyTour

---
+++ ProjectTourism/FormTrangChu.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ProjectTourism
+{
+    partial class FormTrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTrangChu));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTour = new System.Windows.Forms.Button();
+            this.btnLichTour = new System.Windows.Forms.Button();
+            this.btnVe = new System.Windows.Forms.Button();
+            this.btnKHang = new System.Windows.Forms.Button();
+            this.btnHDV = new System.Windows.Forms.Button();
+            this.btnDanhGia = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnDanhGia);
+            this.panel1.Controls.Add(this.btnHDV);
+            this.panel1.Controls.Add(this.btnKHang);
+            this.panel1.Controls.Add(this.btnVe);
+            this.panel1.Controls.Add(this.btnLichTour);
+            this.panel1.Controls.Add(this.btnTour);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnTour
+            // 
+            this.btnTour.AutoSize = true;
+            this.btnTour.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTour.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnTour.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnTour.FlatAppearance.BorderSize = 0;
+            this.btnTour.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnTour.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnTour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTour.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTour.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnTour.Location = new System.Drawing.Point(130, 33);
+            this.btnTour.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTour.Name = "btnTour";
+            this.btnTour.Size = new System.Drawing.Size(76, 29);
+            this.btnTour.TabIndex = 13;
+            this.btnTour.Text = "TOUR";
+            this.btnTour.UseVisualStyleBackColor = false;
+            // 
+            // btnLichTour
+            // 
+            this.btnLichTour.AutoSize = true;
+            this.btnLichTour.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLichTour.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLichTour.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLichTour.FlatAppearance.BorderSize = 0;
+            this.btnLichTour.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnLichTour.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnLichTour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLichTour.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLichTour.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnLichTour.Location = new System.Drawing.Point(210, 33);
+            this.btnLichTour.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLichTour.Name = "btnLichTour";
+            this.btnLichTour.Size = new System.Drawing.Size(107, 29);
+            this.btnLichTour.TabIndex = 14;
+            this.btnLichTour.Text = "LỊCH TOUR";
+            this.btnLichTour.UseVisualStyleBackColor = false;
+            // 
+            // btnVe
+            // 
+            this.btnVe.AutoSize = true;
+            this.btnVe.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnVe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnVe.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnVe.FlatAppearance.BorderSize = 0;
+            this.btnVe.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnVe.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnVe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVe.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnVe.Location = new System.Drawing.Point(308, 33);
+            this.btnVe.Margin = new System.Windows.Forms.Padding(2);
+            this.btnVe.Name = "btnVe";
+            this.btnVe.Size = new System.Drawing.Size(76, 29);
+            this.btnVe.TabIndex = 15;
+            this.btnVe.Text = "VÉ";
+            this.btnVe.UseVisualStyleBackColor = false;
+            // 
+            // btnKHang
+            // 
+            this.btnKHang.AutoSize = true;
+            this.btnKHang.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnKHang.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnKHang.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnKHang.FlatAppearance.BorderSize = 0;
+            this.btnKHang.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnKHang.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnKHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKHang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnKHang.Location = new System.Drawing.Point(384, 33);
+            this.btnKHang.Margin = new System.Windows.Forms.Padding(2);
+            this.btnKHang.Name = "btnKHang";
+            this.btnKHang.Size = new System.Drawing.Size(128, 29);
+            this.btnKHang.TabIndex = 16;
+            this.btnKHang.Text = "KHÁCH HÀNG";
+            this.btnKHang.UseVisualStyleBackColor = false;
+            // 
+            // btnHDV
+            // 
+            this.btnHDV.AutoSize = true;
+            this.btnHDV.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHDV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnHDV.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnHDV.FlatAppearance.BorderSize = 0;
+            this.btnHDV.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnHDV.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnHDV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHDV.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHDV.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnHDV.Location = new System.Drawing.Point(516, 33);
+            this.btnHDV.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHDV.Name = "btnHDV";
+            this.btnHDV.Size = new System.Drawing.Size(162, 29);
+            this.btnHDV.TabIndex = 17;
+            this.btnHDV.Text = "HƯỚNG DẪN VIÊN";
+            this.btnHDV.UseVisualStyleBackColor = false;
+            // 
+            // btnDanhGia
+            // 
+            this.btnDanhGia.AutoSize = true;
+            this.btnDanhGia.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDanhGia.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDanhGia.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnDanhGia.FlatAppearance.BorderSize = 0;
+            this.btnDanhGia.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnDanhGia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnDanhGia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDanhGia.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDanhGia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btnDanhGia.Location = new System.Drawing.Point(690, 33);
+            this.btnDanhGia.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDanhGia.Name = "btnDanhGia";
+            this.btnDanhGia.Size = new System.Drawing.Size(99, 29);
+            this.btnDanhGia.TabIndex = 18;
+            this.btnDanhGia.Text = "ĐÁNH GIÁ";
+            this.btnDanhGia.UseVisualStyleBackColor = false;
+            // 
+            // FormTrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormTrangChu";
+            this.Text = "FormTrangChu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTour;
+        private System.Windows.Forms.Button btnDanhGia;
+        private System.Windows.Forms.Button btnHDV;
+        private System.Windows.Forms.Button btnKHang;
+        private System.Windows.Forms.Button btnVe;
+        private System.Windows.Forms.Button btnLichTour;
+    }
+}
