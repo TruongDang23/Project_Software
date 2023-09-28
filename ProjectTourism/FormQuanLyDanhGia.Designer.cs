10e29161a26e095e2c0ff9a41b6ee48e990344bb
MquanImart <phanminhquanmh@gmail.com>
2023-09-28 12:53:40 +0700

    Thêm cbbMaTour ở FormQuanLyDanhGia

--- ProjectTourism/FormQuanLyDanhGia.Designer.cs
+++ ProjectTourism/FormQuanLyDanhGia.Designer.cs
@@ -35,6 +35,8 @@
             this.dgvQLDanhGia = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbbMaTour = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvQLDanhGia)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -43,15 +45,17 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbbMaTour);
             this.panel1.Controls.Add(this.btnQuayLai);
             this.panel1.Controls.Add(this.btnAVGRate);
             this.panel1.Controls.Add(this.dgvQLDanhGia);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(5, 5);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(924, 398);
+            this.panel1.Size = new System.Drawing.Size(924, 437);
             this.panel1.TabIndex = 4;
             // 
             // btnQuayLai
@@ -61,8 +65,8 @@
             this.btnQuayLai.FlatAppearance.BorderSize = 0;
             this.btnQuayLai.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQuayLai.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnQuayLai.Location = new System.Drawing.Point(664, 340);
-            this.btnQuayLai.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnQuayLai.Location = new System.Drawing.Point(664, 380);
+            this.btnQuayLai.Margin = new System.Windows.Forms.Padding(4);
             this.btnQuayLai.Name = "btnQuayLai";
             this.btnQuayLai.Size = new System.Drawing.Size(232, 48);
             this.btnQuayLai.TabIndex = 25;
@@ -77,8 +81,8 @@
             this.btnAVGRate.FlatAppearance.BorderSize = 0;
             this.btnAVGRate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAVGRate.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnAVGRate.Location = new System.Drawing.Point(443, 340);
-            this.btnAVGRate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAVGRate.Location = new System.Drawing.Point(443, 380);
+            this.btnAVGRate.Margin = new System.Windows.Forms.Padding(4);
             this.btnAVGRate.Name = "btnAVGRate";
             this.btnAVGRate.Size = new System.Drawing.Size(213, 48);
             this.btnAVGRate.TabIndex = 24;
@@ -89,8 +93,8 @@
             // 
             this.dgvQLDanhGia.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(239)))), ((int)(((byte)(247)))));
             this.dgvQLDanhGia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvQLDanhGia.Location = new System.Drawing.Point(32, 121);
-            this.dgvQLDanhGia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvQLDanhGia.Location = new System.Drawing.Point(32, 160);
+            this.dgvQLDanhGia.Margin = new System.Windows.Forms.Padding(4);
             this.dgvQLDanhGia.Name = "dgvQLDanhGia";
             this.dgvQLDanhGia.RowHeadersWidth = 51;
             this.dgvQLDanhGia.Size = new System.Drawing.Size(864, 212);
@@ -100,7 +104,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(32, 4);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(231, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -120,14 +124,34 @@
             this.label1.TabIndex = 11;
             this.label1.Text = "QUẢN LÝ ĐÁNH GIÁ";
             // 
+            // cbbMaTour
+            // 
+            this.cbbMaTour.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbMaTour.FormattingEnabled = true;
+            this.cbbMaTour.Location = new System.Drawing.Point(755, 117);
+            this.cbbMaTour.Name = "cbbMaTour";
+            this.cbbMaTour.Size = new System.Drawing.Size(141, 36);
+            this.cbbMaTour.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.label2.Location = new System.Drawing.Point(659, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 28);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Mã Tour";
+            // 
             // FormQuanLyDanhGia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
-            this.ClientSize = new System.Drawing.Size(936, 407);
+            this.ClientSize = new System.Drawing.Size(936, 446);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormQuanLyDanhGia";
             this.Text = "FormQuanLyDanhGia";
             this.panel1.ResumeLayout(false);
@@ -145,5 +169,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         public System.Windows.Forms.Button btnQuayLai;
         public System.Windows.Forms.Button btnAVGRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbMaTour;
     }
 }
