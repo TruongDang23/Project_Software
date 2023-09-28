ebb762fd8e570abe906f83cd4f635411124d1feb
Suzukisakae <lethanhvinhspk@gmail.com>
2023-09-28 12:59:36 +0700

    Up form tạo chuyến mới

---
+++ ProjectTourism/FormTaoChuyenDiMoi.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ProjectTourism
+{
+    partial class FormTaoChuyenDiMoi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTaoChuyenDiMoi));
+            this.btn_QuayLai = new System.Windows.Forms.Button();
+            this.btn_GuiYeuCau = new System.Windows.Forms.Button();
+            this.dateTimePicker_KhoiHanh = new System.Windows.Forms.DateTimePicker();
+            this.tb_HoVaTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_QuayLai
+            // 
+            this.btn_QuayLai.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btn_QuayLai.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QuayLai.ForeColor = System.Drawing.Color.White;
+            this.btn_QuayLai.Location = new System.Drawing.Point(469, 321);
+            this.btn_QuayLai.Name = "btn_QuayLai";
+            this.btn_QuayLai.Size = new System.Drawing.Size(126, 45);
+            this.btn_QuayLai.TabIndex = 106;
+            this.btn_QuayLai.Text = "Quay lại";
+            this.btn_QuayLai.UseVisualStyleBackColor = false;
+            // 
+            // btn_GuiYeuCau
+            // 
+            this.btn_GuiYeuCau.BackColor = System.Drawing.Color.White;
+            this.btn_GuiYeuCau.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_GuiYeuCau.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.btn_GuiYeuCau.Location = new System.Drawing.Point(298, 323);
+            this.btn_GuiYeuCau.Name = "btn_GuiYeuCau";
+            this.btn_GuiYeuCau.Size = new System.Drawing.Size(151, 45);
+            this.btn_GuiYeuCau.TabIndex = 105;
+            this.btn_GuiYeuCau.Text = "Gửi yêu cầu";
+            this.btn_GuiYeuCau.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker_KhoiHanh
+            // 
+            this.dateTimePicker_KhoiHanh.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(239)))), ((int)(((byte)(247)))));
+            this.dateTimePicker_KhoiHanh.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(239)))), ((int)(((byte)(247)))));
+            this.dateTimePicker_KhoiHanh.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker_KhoiHanh.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateTimePicker_KhoiHanh.Location = new System.Drawing.Point(260, 203);
+            this.dateTimePicker_KhoiHanh.Name = "dateTimePicker_KhoiHanh";
+            this.dateTimePicker_KhoiHanh.Size = new System.Drawing.Size(293, 31);
+            this.dateTimePicker_KhoiHanh.TabIndex = 104;
+            // 
+            // tb_HoVaTen
+            // 
+            this.tb_HoVaTen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(239)))), ((int)(((byte)(247)))));
+            this.tb_HoVaTen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_HoVaTen.Location = new System.Drawing.Point(260, 252);
+            this.tb_HoVaTen.Multiline = true;
+            this.tb_HoVaTen.Name = "tb_HoVaTen";
+            this.tb_HoVaTen.Size = new System.Drawing.Size(90, 36);
+            this.tb_HoVaTen.TabIndex = 103;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(32, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 36);
+            this.label2.TabIndex = 102;
+            this.label2.Text = "Số lượng:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Black;
+            this.label12.Location = new System.Drawing.Point(32, 203);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(222, 36);
+            this.label12.TabIndex = 101;
+            this.label12.Text = "Ngày khởi hành:";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(100)))), ((int)(((byte)(171)))));
+            this.label1.Location = new System.Drawing.Point(197, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "TẠO CHUYẾN MỚI";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(164, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 99;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormTaoChuyenDiMoi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(627, 377);
+            this.Controls.Add(this.btn_QuayLai);
+            this.Controls.Add(this.btn_GuiYeuCau);
+            this.Controls.Add(this.dateTimePicker_KhoiHanh);
+            this.Controls.Add(this.tb_HoVaTen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormTaoChuyenDiMoi";
+            this.Text = "FormTaoChuyenDiMoi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_QuayLai;
+        private System.Windows.Forms.Button btn_GuiYeuCau;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_KhoiHanh;
+        private System.Windows.Forms.TextBox tb_HoVaTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
