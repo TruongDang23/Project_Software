c8d5477ba1019e99b515102d58f955c9f8150549
ThanhHung0203 <hungat418@gmail.com>
2023-10-12 10:02:29 +0700

    Chinh lai giao dien FormChiTietChuyenDi, FormDatChuyenDi

--- ProjectTourism/FormDatChuyenDi.cs
+++ ProjectTourism/FormDatChuyenDi.cs
@@ -1,11 +1,9 @@
-﻿using ProjectTourism.BSLayer;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,7 +15,7 @@ namespace ProjectTourism
         private BLUser tasks = new BLUser();
         private string MaChuyenDi;
         private DateTime NgayBatDau;
-        public FormDatChuyenDi(string MaChuyenDi, DateTime NgayBatDau)
+        private string MaTaiKhoan;
         {
             InitializeComponent();
             this.MaChuyenDi = MaChuyenDi;
