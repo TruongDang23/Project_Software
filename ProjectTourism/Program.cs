af0d9104a67df8d2c070d63b92fe6bdaf70ee5f6
TruongDang23 <quantruong050123@gmail.com>
2023-10-16 07:56:26 +0700

    Pull Code

--- ProjectTourism/Program.cs
+++ ProjectTourism/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectTourism
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormQuanLyDanhGia());
+            Application.Run(new FormHuongDanVien());
         }
     }
 }
